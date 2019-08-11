ddd9bb4afd5da312d525736e134a9986aefaeeab
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-11 12:59:59 +0200

    list + item + first fetch

---
+++ src/components/TicketElement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ticketelementcontainer">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TicketElement',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+</style>
---
+++ src/components/TicketList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <ticket-element v-for="ticket of tickets" v-bind:key="concert.date"
+                    v-bind:ticket="ticket"></ticket-element>
+  </div>
+</template>
+
+<script>
+  import TicketElement from "./TicketElement";
+  import axios from "axios";
+
+  export default {
+    name: 'TicketList',
+    components: {TicketElement},
+    props: {
+      concerturi: String
+    },
+
+    data: function () {
+      return {
+        resultData: '',
+        concerts: ''
+      }
+    },
+
+    created() {
+      this.getTickets();
+    },
+
+    getTickets() {
+      axios.get(`https://api.marcoreitano.dev/tickets/search/findByConcert`, {
+        params: {
+          concerturi: this.concerturi
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+</style>
