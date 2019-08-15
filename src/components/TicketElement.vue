ee496377e31dc76d5c423066020b8e80ee6c736f
Marco Reitano <marco.reitano@th-koeln.de>
2019-08-15 17:16:31 +0200

    waaaah

--- src/components/TicketElement.vue
+++ src/components/TicketElement.vue
@@ -11,8 +11,8 @@
       </button>
     </div>
     <div v-else>
-      <order-add-cart-button ticketuri={{this.ticket._links.self.href}}
-                             available={{this.ticket.availability}}></order-add-cart-button>
+      <order-add-cart-button :ticketuri="this.ticket._links.self.href"
+                             :available="this.ticket.availability"></order-add-cart-button>
     </div>
   </div>
 </template>
